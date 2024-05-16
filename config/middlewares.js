4bd89c75dead1e7017ad0a3c0c3bab2ec0d0ae1e
Vanya <dev@tokenville.io>
2024-05-16 15:40:39 +0300

    middleware fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,46 +1,43 @@
-    // ~/strapi-aws-s3/backend/config/middlewares.js
-    
-    module.exports = [
-    {
-      settings: {
-        cors: {
-          enabled: true,
-          origin: ['https://8d-1.com', 'https://www.8d-1.com', 'https://api.8d-1.com', 'https://www.api.8d-1.com', 'http://localhost:3000'], 
-          headers: ['Content-Type', 'Authorization', 'X-Frame-Options'],
+module.exports = [
+  {
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ['https://8d-1.com', 'https://www.8d-1.com', 'https://api.8d-1.com', 'https://www.api.8d-1.com', 'http://localhost:3000'], 
+        headers: ['Content-Type', 'Authorization', 'X-Frame-Options'],
+      },
+    },
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi8d1.s3.eu-north-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi8d1.s3.eu-north-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
         },
       },
-        name: 'strapi::security',
-        config: {
-          contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-              'connect-src': ["'self'", 'https:'],
-              'img-src': [
-                "'self'",
-                'data:',
-                'blob:',
-                'dl.airtable.com',
-                'strapi8d1.s3.eu-north-1.amazonaws.com',
-              ],
-              'media-src': [
-                "'self'",
-                'data:',
-                'blob:',
-                'dl.airtable.com',
-                'strapi8d1.s3.eu-north-1.amazonaws.com',
-              ],
-              upgradeInsecureRequests: null,
-            },
-          },
-        },
-      },
-      /* End of snippet */
-      'strapi::cors',
-      'strapi::poweredBy',
-      'strapi::logger',
-      'strapi::query',
-      'strapi::body',
-      'strapi::session',
-      'strapi::favicon',
-      'strapi::public',
-    ];
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
