96046db69b6c0546210cdc7e86d9ec26fe6c558b
Vanya <dev@tokenville.io>
2025-03-17 15:50:13 +0200

    remove isOwner middleware from assistant and interview APIs

--- src/api/interview/routes/interview.js
+++ src/api/interview/routes/interview.js
@@ -1,18 +1,3 @@
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter("api::interview.interview", {
-  config: {
-    update: {
-      middlewares: ["api::interview.is-owner"],
-    },
-    delete: {
-      middlewares: ["api::interview.is-owner"],
-    },
-    find: {
-      middlewares: ["api::interview.is-owner"],
-    },
-    findOne: {
-      middlewares: ["api::interview.is-owner"],
-    },
-  },
-});
+module.exports = createCoreRouter("api::interview.interview");
